681bee3aca90b8240ea379850b185389ed937f8d
ThreeDP <davypaulinocbjr@outlook.com>
2022-10-24 22:37:07 +0000

    Implemented solution more fast for the algorithm max pairwise product

---
+++ srcs/max_pairwise_product_fast.c
@@ -0,0 +1,28 @@
+#include "lib_alg.h"
+
+static int	cmp_indx(const size_t *numbers, size_t actual, size_t max)
+{
+	if (numbers[actual] == numbers[max])
+		return (1);
+	return (0);
+}
+
+size_t	max_pairwise_product_fast(const size_t *numbers, size_t size)
+{
+	size_t	frt_indx;
+	size_t	sec_indx;
+	size_t	i;
+
+	i = 0;
+	frt_indx = -1;
+	while (i < size)
+		if ((frt_indx == -1) || (nums[i] > nums[frt_indx]))
+			frt_indx = i;
+	i = 0;
+	sec_indx = -1;
+	while (i < size)	
+		if (cmp_indx(nums, i, frt_indx)
+		&& (sec_indx == -1) || (nums[i] > nums[sec_indx]))
+			sec_indx = i;
+	return (nums[frt_indx] * nums[sec_indx]);
+}
