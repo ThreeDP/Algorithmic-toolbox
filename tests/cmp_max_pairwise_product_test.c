98300db52e543580e18ac0cabb7cb2e53c73e0a5
Threedp <davy.paulino@aluno.ifsp.edu.br>
2022-11-03 11:58:11 -0300

    Implementation of test for compare alg, and new minunit test for time

---
+++ tests/cmp_max_pairwise_product_test.c
@@ -0,0 +1,73 @@
+#include "../srcs/lib_alg.h"
+#include "minunit.h"
+#include "time.h"
+
+MU_TEST_SUITE(passing_6_elements_and_numbers_1_4_5_25_60_3_shouuld_be_1500)
+{
+	//ARRANGE
+	int		num_elements;
+	size_t	array[] = {1, 4, 5, 25, 60, 3};
+	size_t	slow;
+	size_t	fast;
+	time_t	start;
+	time_t	end;
+	time_t	t_fast;
+	time_t	t_slow;
+
+	//ACT
+	num_elements = 6;
+	time(&start);
+	slow = max_pairwise_product(array, num_elements);
+	time(&end);
+	t_slow = start - end;
+	time(&start);
+	fast = max_pairwise_product_fast(array, num_elements);
+	time(&end);
+	t_fast = start - end;
+
+	//ASSERT
+	mu_assert_int_eq(fast, slow);
+	mu_assert_time(t_fast, t_slow);
+}
+
+MU_TEST_SUITE(passing_2_elements_and_numbers_100000_90000_shouuld_be_9000000000)
+{
+	//ARRANGE
+	int		num_elements;
+	size_t	array[] = {100000, 90000};
+	size_t	expected_result;
+	size_t	result;
+
+	//ACT
+	num_elements = 2;
+	expected_result = 9000000000;
+	result = max_pairwise_product_fast(array, num_elements);
+
+	//ASSERT
+	mu_assert_int_eq(expected_result, result);
+}
+
+MU_TEST_SUITE(passing_10_elements_and_numbers_7_5_14_2_8_8_10_1_2_3_should_be_140)
+{
+	//ARRANGE
+	int		num_elements;
+	size_t	array[] = {7, 5, 14, 2, 8, 8, 10, 1, 2, 3};
+	size_t	expected_result;
+	size_t	result;
+
+	//ACT
+	num_elements = 10;
+	expected_result = 140;
+	result = max_pairwise_product_fast(array, num_elements);
+
+	//ASSERT
+	mu_assert_int_eq(expected_result, result);
+}
+
+int main() {
+	MU_RUN_TEST(passing_10_elements_and_numbers_7_5_14_2_8_8_10_1_2_3_should_be_140);
+	MU_RUN_TEST(passing_2_elements_and_numbers_100000_90000_shouuld_be_9000000000);
+	MU_RUN_TEST(passing_6_elements_and_numbers_1_4_5_25_60_3_shouuld_be_1500);
+	MU_REPORT();
+	return MU_EXIT_CODE;
+}
--- tests/minunit.h
+++ tests/minunit.h
@@ -211,6 +211,21 @@ static void (*minunit_teardown)(void) = NULL;
 	}\
 )
 
+#define mu_assert_time(fast, slow) MU__SAFE_BLOCK(\
+	double minunit_tmp_e;\
+	double minunit_tmp_r;\
+	minunit_assert++;\
+	minunit_tmp_e = (start);\
+	minunit_tmp_r = (end);\
+	if (minunit_tmp_e > minunit_tmp_r) {\
+		printf(BOLDGREEN(".'"));\
+	} else {\
+		snprintf(minunit_last_message, MINUNIT_MESSAGE_LEN, PRINTRED("%s failed:\n\t%s:%d: %.*g expected but was %.*g"), __func__, __FILE__, __LINE__, minunit_tmp_e, minunit_tmp_r);\
+		minunit_status = 1;\
+		return;\
+	}\
+)
+
 #define mu_assert_string_eq(expected, result) MU__SAFE_BLOCK(\
 	const char* minunit_tmp_e = expected;\
 	const char* minunit_tmp_r = result;\
