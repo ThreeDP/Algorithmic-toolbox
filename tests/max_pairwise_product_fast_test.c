fd5095ee1a135484ddce1bdf8e16520058a74197
Threedp <davy.paulino@aluno.ifsp.edu.br>
2022-11-03 10:30:08 -0300

    last

--- tests/max_pairwise_product_fast_test.c
+++ tests/max_pairwise_product_fast_test.c
@@ -7,7 +7,6 @@ MU_TEST_SUITE(passing_6_elements_and_numbers_1_4_5_25_60_3_shouuld_be_1500)
 {
 	//ARRANGE
 	int		i;
-	size_t		*ptr;
 	int		num_elements;
 	size_t	array[] = {1, 4, 5, 25, 60, 3};
 	size_t	expected_result;
@@ -17,24 +16,16 @@ MU_TEST_SUITE(passing_6_elements_and_numbers_1_4_5_25_60_3_shouuld_be_1500)
 	i = 0;
 	num_elements = 6;
 	expected_result = 1500;
-	ptr = calloc(num_elements + 1, sizeof(expected_result));
-	while (i < num_elements)
-	{
-		ptr[i] = array[i];
-		i++;
-	}
-	result = max_pairwise_product_fast(ptr, num_elements);
+	result = max_pairwise_product_fast(array, num_elements);
 
 	//ASSERT
 	mu_assert_int_eq(expected_result, result);
-	free(ptr);
 }
 
 MU_TEST_SUITE(passing_2_elements_and_numbers_100000_90000_shouuld_be_9000000000)
 {
 	//ARRANGE
 	int		i;
-	size_t		*ptr;
 	int		num_elements;
 	size_t	array[] = {100000, 90000};
 	size_t	expected_result;
@@ -44,24 +35,16 @@ MU_TEST_SUITE(passing_2_elements_and_numbers_100000_90000_shouuld_be_9000000000)
 	i = 0;
 	num_elements = 2;
 	expected_result = 9000000000;
-	ptr = calloc(num_elements + 1, sizeof(expected_result));
-	while (i < num_elements)
-	{
-		ptr[i] = array[i];
-		i++;
-	}
-	result = max_pairwise_product_fast(ptr, num_elements);
+	result = max_pairwise_product_fast(array, num_elements);
 
 	//ASSERT
 	mu_assert_int_eq(expected_result, result);
-	free(ptr);
 }
 
 MU_TEST_SUITE(passing_10_elements_and_numbers_7_5_14_2_8_8_10_1_2_3_should_be_140)
 {
 	//ARRANGE
 	int		i;
-	size_t		*ptr;
 	int		num_elements;
 	size_t	array[] = {7, 5, 14, 2, 8, 8, 10, 1, 2, 3};
 	size_t	expected_result;
@@ -71,17 +54,10 @@ MU_TEST_SUITE(passing_10_elements_and_numbers_7_5_14_2_8_8_10_1_2_3_should_be_14
 	i = 0;
 	num_elements = 10;
 	expected_result = 140;
-	ptr = calloc(num_elements + 1, sizeof(expected_result));
-	while (i < num_elements)
-	{
-		ptr[i] = array[i];
-		i++;
-	}
-	result = max_pairwise_product_fast(ptr, num_elements);
+	result = max_pairwise_product_fast(array, num_elements);
 
 	//ASSERT
 	mu_assert_int_eq(expected_result, result);
-	free(ptr);
 }
 
 int main() {
