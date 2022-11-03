290d0ceedc5a615b04db98d9ae0c91b313516200
Threedp <davy.paulino@aluno.ifsp.edu.br>
2022-11-03 12:39:56 -0300

    Implementation of test for compare alg, and new minunit test for time

--- tests/cmp_max_pairwise_product_test.c
+++ tests/cmp_max_pairwise_product_test.c
@@ -19,7 +19,7 @@ MU_TEST_SUITE(passing_6_elements_and_numbers_1_4_5_25_60_3_shouuld_be_1500)
 	time(&start);
 	slow = max_pairwise_product(array, num_elements);
 	time(&end);
-	printf("\ns\t%zu\t%f\ne\t%zu\t%f\n", start, (double)start / CLOCKS_PER_SEC, end, (double)end / CLOCKS_PER_SEC);
+	printf("\ns\t%zu\t%.20f\ne\t%zu\t%.20f\n", start, (double)start / CLOCKS_PER_SEC, end, (double)end / CLOCKS_PER_SEC);
 	t_slow = (double)(end - start) / CLOCKS_PER_SEC;
 	printf("\n%.20f", t_slow);
 	time(&start);
