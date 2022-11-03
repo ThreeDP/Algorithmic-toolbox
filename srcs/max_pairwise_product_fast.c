0e70e63e72618413c0de70b8b26c0fcd37f0e15c
ThreeDP <davypaulinocbjr@outlook.com>
2022-11-03 13:10:25 +0000

    new

--- srcs/max_pairwise_product_fast.c
+++ srcs/max_pairwise_product_fast.c
@@ -1,8 +1,8 @@
 #include "lib_alg.h"
 
-static int	cmp_indx(const size_t *numbers, int actual, int max)
+int	cmp_indx(const size_t *numbers, int actual, int max)
 {
-	if (numbers[actual] == numbers[max])
+	if (!(numbers[actual] == numbers[max]))
 		return (1);
 	return (0);
 }
@@ -16,16 +16,13 @@ size_t	max_pairwise_product_fast(const size_t *nums, int size)
 	i = 0;
 	frt_indx = -1;
 	while (i < size)
-	{
-		printf("\t%i", i);
 		if (nums[i++] > nums[frt_indx] || (frt_indx == -1))
 			frt_indx = i;
-	}
 	i = 0;
 	sec_indx = -1;
-	while (i < size)	
-		if (cmp_indx(nums, i, frt_indx) &&
-				((nums[i++] > nums[sec_indx]) || (sec_indx == -1)))
+	while (i < size)
+		if ((nums[i++] > nums[sec_indx] || (sec_indx == -1)) && cmp_indx(nums, i, frt_indx))
 			sec_indx = i;
+	printf("\nMax\t%zu\nSec\t%zu\n", nums[frt_indx], nums[sec_indx]);
 	return (nums[frt_indx] * nums[sec_indx]);
 }
