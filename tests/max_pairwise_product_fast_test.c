b3d16def949830ac93dbe474f2f6767982e07d60
Threedp <davy.paulino@aluno.ifsp.edu.br>
2022-11-03 10:31:16 -0300

    last

--- tests/max_pairwise_product_fast_test.c
+++ tests/max_pairwise_product_fast_test.c
@@ -6,14 +6,12 @@ size_t	max_pairwise_product(const size_t *numbers, size_t size);
 MU_TEST_SUITE(passing_6_elements_and_numbers_1_4_5_25_60_3_shouuld_be_1500)
 {
 	//ARRANGE
-	int		i;
 	int		num_elements;
 	size_t	array[] = {1, 4, 5, 25, 60, 3};
 	size_t	expected_result;
 	size_t	result;
 
 	//ACT
-	i = 0;
 	num_elements = 6;
 	expected_result = 1500;
 	result = max_pairwise_product_fast(array, num_elements);
@@ -25,14 +23,12 @@ MU_TEST_SUITE(passing_6_elements_and_numbers_1_4_5_25_60_3_shouuld_be_1500)
 MU_TEST_SUITE(passing_2_elements_and_numbers_100000_90000_shouuld_be_9000000000)
 {
 	//ARRANGE
-	int		i;
 	int		num_elements;
 	size_t	array[] = {100000, 90000};
 	size_t	expected_result;
 	size_t	result;
 
 	//ACT
-	i = 0;
 	num_elements = 2;
 	expected_result = 9000000000;
 	result = max_pairwise_product_fast(array, num_elements);
@@ -44,14 +40,12 @@ MU_TEST_SUITE(passing_2_elements_and_numbers_100000_90000_shouuld_be_9000000000)
 MU_TEST_SUITE(passing_10_elements_and_numbers_7_5_14_2_8_8_10_1_2_3_should_be_140)
 {
 	//ARRANGE
-	int		i;
 	int		num_elements;
 	size_t	array[] = {7, 5, 14, 2, 8, 8, 10, 1, 2, 3};
 	size_t	expected_result;
 	size_t	result;
 
 	//ACT
-	i = 0;
 	num_elements = 10;
 	expected_result = 140;
 	result = max_pairwise_product_fast(array, num_elements);
