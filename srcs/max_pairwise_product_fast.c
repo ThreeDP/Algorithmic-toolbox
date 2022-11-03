95f0fa417ba2f18154a5c0f4379b31120a13167c
Threedp <davy.paulino@aluno.ifsp.edu.br>
2022-11-03 09:24:10 -0300

    i

--- srcs/max_pairwise_product_fast.c
+++ srcs/max_pairwise_product_fast.c
@@ -16,13 +16,13 @@ size_t	max_pairwise_product_fast(const size_t *nums, int size)
 	i = 0;
 	frt_indx = -1;
 	while (i < size)
-		if ((frt_indx == -1) || (nums[i] > nums[frt_indx]))
+		if ((frt_indx == -1) || (nums[i++] > nums[frt_indx]))
 			frt_indx = i;
 	i = 0;
 	sec_indx = -1;
 	while (i < size)	
 		if (cmp_indx(nums, i, frt_indx) &&
-				((sec_indx == -1) || (nums[i] > nums[sec_indx])))
+				((sec_indx == -1) || (nums[i++] > nums[sec_indx])))
 			sec_indx = i;
 	return (nums[frt_indx] * nums[sec_indx]);
 }
